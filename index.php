93c3e847e2b3c7b3888f22e6de026a1169ae5870
yves <kabiryves90@gmail.com>
2020-07-21 19:44:14 +0200

    les varibles

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>devoir des bases du phph</title>
+</head>
+<body>
+<?php
+ //les varibles
+
+$entier=5;
+$entiers=-5;
+$caract="hello";
+$decimal=5.6;
+
+echo $entier.'<br/>';
+echo $entiers.'<br/>';
+echo $caract.'<br/>'; 
+echo $decimal.'<br/>';
+echo "1 a 2 ------------------------------------------";
+echo "---- ------------------------------------------ <br/>";
+
+echo "<br/>";
+echo " <br/>";
+/*Les opérateurs arithmétiques
+
+$a=3;
+$b=7;
+$c=4;
+$d=9;
+
+echo $a + $c."<br/>";           
+echo $d - $b."<br/>";          
+echo $d / $a."<br/>";          
+echo $b % $c."<br/>";         
+
+echo "---- ------------------------------------------ <br/>";
+
+echo "<br/>";
+echo " <br/>";*/
+
+
+
+?>
+</body>
+</html>
